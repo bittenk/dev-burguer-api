77b787f069e26dcc0a095f67bda407f3069bd2c6
ERICK BITTENCOURT DOS SANTOS <erickbittencourt1812@gmail.com>
2026-06-23 16:20:56 -0300

    fix: adiciona blocos de ambiente development e production no sequelize

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,14 +1,32 @@
+require('dotenv').config();
+
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'Erk300163150421.',
-  database: 'postgres',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  development: {
+    username: 'postgres',
+    password: 'Erk300163150421.',
+    database: 'postgres',
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5432,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+  production: {
+    url: process.env.DATABASE_URL, // Vai ler a variável do Supabase salva no Render
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Exigido pelo Supabase para conexões externas seguras
+      },
+    },
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
   },
 };
-
